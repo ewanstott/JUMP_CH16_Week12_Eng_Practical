7598f6dab1e9c5827d74c32028ab64c26690fd41
Ewan Stott <ewan.stott1@gmail.com>
2023-12-14 14:54:05 +0000

    commit

--- array_practice_problem.js
+++ array_practice_problem.js
@@ -22,16 +22,47 @@
 // 	}]
 // }]
 
-const treeDictionary = {};
+// const treeDictionary = {};
 
-for (i = 0; i < Array.length; i++) {}
+// for (i = 0; i < Array.length; i++) {}
 
-class Tree {
-  constructor() {
-    this.root = null;
-  }
+// class Tree {
+//   constructor() {
+//     this.root = null;
+//   }
 
-  add(data) {
-    this.children.push(new Node(data));
-  }
-}
+//   add(data) {
+//     this.children.push(new Node(data));
+//   }
+// }
+
+//Array method practice
+
+//MAP
+// const finalParticipants = ["Taylor", "Donald", "Don", "Natasha", "Bobby"];
+
+// // add string after each final participant
+
+// const announcements = finalParticipants.map((p) => {
+//   return p + " joined the party.";
+// });
+
+// console.log(announcements);
+
+// Map
+// const list = [1, 2, 3, 4];
+// console.log(list.map((num) => num * 2)); // [2, 4, 6, 8]
+
+//filter - Returns a new array with all elements that pass the test implemented by the provided function.
+// const randomNumbers = [4, 11, 42, 14, 39];
+// const returnedNums = randomNumbers.filter((x) => {
+//   return x % 2 === 0;
+// });
+// console.log(returnedNums);
+
+//REDUCE - Reduce the array to a single value. The value returned by the function is stored in an accumulator (result/total).
+// const arrayOfNumbers = [1, 2, 3, 4];
+// const reducedNums = arrayOfNumbers.reduce((total, current) => {
+//   return total + current;
+// });
+// console.log(reducedNums);
